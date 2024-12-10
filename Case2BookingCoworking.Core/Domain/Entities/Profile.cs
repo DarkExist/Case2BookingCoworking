81aceb5bd6c67654712767d5f641dab4a47f7032
Даниил Поминов <d.pominov2016@yandex.ru>
2024-12-10 18:13:25 +0700

    Рабочая регистрация

--- Case2BookingCoworking.Core/Domain/Entities/Profile.cs
+++ Case2BookingCoworking.Core/Domain/Entities/Profile.cs
@@ -4,6 +4,13 @@ namespace Case2BookingCoworking.Core.Domain.Entities
 {
     public class Profile : Entity<Guid>
     {
+        public Profile() { 
+            Id = Guid.NewGuid(); 
+            FullCredential = string.Empty;
+            TelegramId = string.Empty;
+            PhoneNumber = string.Empty;
+            UniversityEmail = string.Empty;
+        }
         public User User { get; set; }
         public Guid UserId { get; set; }
         public string FullCredential { get; set; }
