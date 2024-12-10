634f6713c7446024bcdc07c70d3be2e1cfc50027
Даниил Поминов <d.pominov2016@yandex.ru>
2024-12-10 11:27:56 +0700

    Добавил jwt, шифратор, заготовки для persistance

---
+++ Case2BookingCoworking.Infrastructure/Data/Context/BookingContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace Case2BookingCoworking.Infrastructure.Data.Context
+{
+    public class BookingContext : DbContext
+    {
+        protected readonly IConfiguration? _configuration;
+
+        public BookingContext(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+
+            var options = _configuration?.GetRequiredSection("ConnectionStrings");
+            optionsBuilder.UseNpgsql(options?.GetRequiredSection("BookingApp").Value ?? "server=localhost, username=pgdamin")
+                .UseLoggerFactory(CreateLoggerFactory())
+                .EnableSensitiveDataLogging();
+        }
+        public ILoggerFactory CreateLoggerFactory() => LoggerFactory
+            .Create(builder => { builder.AddConsole(); });
+    }
+}
---
+++ Case2BookingCoworking.Infrastructure/Security/PasswordHasher.cs
@@ -0,0 +1,17 @@
+﻿using Case2BookingCoworking.Application.Abstract;
+
+namespace DODQuiz.Infrastructure.Security
+{
+    internal class PasswordHasher : IPasswordHasher
+    {
+        public string Generate(string password)
+        {
+            return BCrypt.Net.BCrypt.EnhancedHashPassword(password);
+        }
+
+        public bool VerifyPassword(string password, string HashedPassword)
+        {
+            return BCrypt.Net.BCrypt.EnhancedVerify(password, HashedPassword);
+        }
+    }
+}
